dc96d2465af4406f783f55e9cb146f11cd0dcc17
spaceark7 <spacearkchief@gmail.com>
2024-06-18 22:33:42 +0700

    feat: Add support for member-detail and add-member screens in RenderScreen component

--- layouts/render-screen.tsx
+++ layouts/render-screen.tsx
@@ -1,13 +1,25 @@
 import EditCitizenshipForm from '@/components/ui/profile/EditCitizenship'
 import EditForm from '@/components/ui/profile/EditForm'
+import AddMember from '@/components/ui/screens/admin/members/add-member'
+import MemberDetailAdmin from '@/components/ui/screens/admin/members/member-detail-admin'
 
-const RenderScreen = ({ segment }: { segment: string }) => {
+const RenderScreen = ({
+  segment,
+  ...props
+}: {
+  segment: string
+  [key: string]: any
+}) => {
   console.log('segment', segment)
   switch (segment) {
     case 'profile':
       return <EditForm />
     case 'citizenship':
       return <EditCitizenshipForm />
+    case 'member-detail':
+      return <MemberDetailAdmin memberId={props.memberId} {...props} />
+    case 'add-member':
+      return <AddMember {...props} />
     default:
       return null
   }
