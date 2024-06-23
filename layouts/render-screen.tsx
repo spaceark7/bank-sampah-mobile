d477ca4f2367ea51b6ee3e06aa49f2c6d3fc04df
spaceark7 <spacearkchief@gmail.com>
2024-06-23 22:24:51 +0700

    feat: Add EditMemberCitizenship component for editing member citizenship

--- layouts/render-screen.tsx
+++ layouts/render-screen.tsx
@@ -1,6 +1,9 @@
 import EditCitizenshipForm from '@/components/ui/profile/EditCitizenship'
 import EditForm from '@/components/ui/profile/EditForm'
+import EditMemberCitizenship from '@/components/ui/profile/edit-member-citizenship'
+import EditMemberForm from '@/components/ui/profile/edit-member-form'
 import ProfileView from '@/components/ui/profile/profile-view'
+import MaterialForm from '@/components/ui/screens/admin/materials/material-form'
 import MemberDetailAdmin from '@/components/ui/screens/admin/members/member-detail-admin'
 import RegisterMember from '@/components/ui/screens/admin/members/member-register'
 
@@ -15,6 +18,10 @@ const RenderScreen = ({
   switch (segment) {
     case 'profile':
       return <EditForm />
+    case 'profile-member':
+      return <EditMemberForm memberId={props.memberId} />
+    case 'citizenship-member':
+      return <EditMemberCitizenship memberId={props.memberId} />
     case 'citizenship':
       return <EditCitizenshipForm />
     case 'member-detail':
@@ -23,6 +30,8 @@ const RenderScreen = ({
       return <ProfileView memberId={props.memberId} {...props} />
     case 'add-member':
       return <RegisterMember {...props} />
+    case 'add-material':
+      return <MaterialForm {...props} />
     default:
       return null
   }
